5bf1b9f7b62fad98c3d8b6f40ff3a9512ba4f919
stxve4203 <stefan.weiss1@aol.com>
2024-03-27 11:43:22 +0100

    added is_extruder_used variable

--- src/libslic3r/GCode.cpp
+++ src/libslic3r/GCode.cpp
@@ -1624,6 +1624,13 @@ void GCode::_do_export(Print& print_mod, GCodeOutputStream &file, ThumbnailsGene
         m_placeholder_parser.set("first_layer_print_min",  new ConfigOptionFloats({ bbox.min.x(), bbox.min.y() }));
         m_placeholder_parser.set("first_layer_print_max",  new ConfigOptionFloats({ bbox.max.x(), bbox.max.y() }));
         m_placeholder_parser.set("first_layer_print_size", new ConfigOptionFloats({ bbox.size().x(), bbox.size().y() }));
+        this->placeholder_parser().set("num_extruders", int(print.config().nozzle_diameter.values.size()));
+        std::vector<unsigned char> is_extruder_used(std::max(size_t(255), print.config().nozzle_diameter.size()), 0);
+        for (unsigned int extruder_id : tool_ordering.all_extruders())
+            is_extruder_used[extruder_id] = true;
+        this->placeholder_parser().set("is_extruder_used", new ConfigOptionBools(is_extruder_used));
+        
+        
     } else {
         //have to compute it ourself :-/
         class BoundingBoxVisitor : public ExtrusionVisitorRecursiveConst {
